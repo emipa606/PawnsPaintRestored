654657acc835d67fb8ceaad82e9343211bdfcb77
Mlie <emipa606@users.noreply.github.com>
2022-05-12 20:39:37 +0200

    Added wall-hung paintings, sligntly smaller in size

---
+++ Source/PawnsPaintRestored/Painting_PlaceWorker.cs
@@ -0,0 +1,48 @@
+﻿using Verse;
+
+namespace PawnsPaintRestored;
+
+public class Painting_PlaceWorker : PlaceWorker
+{
+    public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map,
+        Thing thingToIgnore = null, Thing thing = null)
+
+    {
+        var c = loc;
+
+        var support = c.GetEdifice(map);
+        if (support == null)
+        {
+            return "PPR.onsupport".Translate();
+        }
+
+        if (support.def?.graphicData == null)
+        {
+            return "PPR.onsupport".Translate();
+        }
+
+        if ((support.def.graphicData.linkFlags & (LinkFlags.Rock | LinkFlags.Wall)) == 0)
+        {
+            return "PPR.onsupport".Translate();
+        }
+
+        c = loc + IntVec3.South;
+        if (!c.Walkable(map))
+        {
+            return "PPR.walkable".Translate();
+        }
+
+        var currentBuildings = loc.GetThingList(map);
+        foreach (var building in currentBuildings)
+        {
+            if (building?.def?.defName != "WallmountedBattery")
+            {
+                continue;
+            }
+
+            return "PPR.existing".Translate();
+        }
+
+        return AcceptanceReport.WasAccepted;
+    }
+}
